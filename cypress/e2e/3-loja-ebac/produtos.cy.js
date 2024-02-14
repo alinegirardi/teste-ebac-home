90954d21246cc317558d864ac3deb2f8aa073536
Aline Girardi <alinegirardiunb@gmail.com>
2024-02-14 19:38:40 -0300

    listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,17 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+            //.first().click()
+            //.last().click()
+            //.eq(2).click()
+            .contains('Ariel Roll Sleeve Sweatshirt').click()
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+});
